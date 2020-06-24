99a65a93777ae1da7fd490dcd8dfbcbee9f4a1a0
Zach H <55612469+zjhans@users.noreply.github.com>
2020-06-24 09:17:18 -0400

    Create OpenShiftNamespacePodReport.sh

---
+++ OpenShiftNamespacePodReport.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+today=$(date +'%Y%m%d')
+if [ -f /root/ClusterInventory/AllActiveNamespaces.txt ]; then
+     rm -f /root/ClusterInventory/AllActiveNamespaces.txt
+fi
+
+oc get namespaces | awk '{print $1}' | grep -v NAME >> AllActiveNamespaces.txt
+for i in `cat AllActiveNamespaces.txt`
+     do PODCNT=`oc get pods -n $i | grep -v READY | grep 'Running\|Completed' | wc -l`
+     if [ "$PODCNT" -ge "1" ]
+     then
+          echo "$i" >> PopulatedNamespaces_$today.csv
+    else 
+          echo "$i" >> EmptyNamespaces_$today.csv
+     fi
+done
