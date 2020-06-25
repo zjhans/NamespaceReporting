470d75360fd13e30d82e02b28167a5205c68860d
Zach H <55612469+zjhans@users.noreply.github.com>
2020-06-25 08:57:23 -0400

    Update NamespaceReportDL.sh

--- NamespaceReportDL.sh
+++ NamespaceReportDL.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 today=$(date +'%Y%m%d')
-scp username@clustername.comcast.net:/home/username/path/EmptyNamespaces_$today.csv /home/username/Documents/
-scp username@clustername.comcast.net:/home/username/path/PopulatedNamespaces_$today.csv /home/username/Documents/
+yesterday=$(date -d "yesterday 11:00" '+%Y%m%d')
+
+# Cleanup yesterday's run
+if [ -d "/home/username/ClusterInventories/AllClusters_$yesterday" ]; then
+	rm -rf /home/username/ClusterInventories/AllClusters_$yesterday
+fi
+
+# Make a new dir for today's run
+mkdir /home/username/ClusterInventories/AllClusters_$today
+
+# Copy down all pod/namespaces reports from the clusters
+scp user@hostname1.comcast.net:/root/ClusterInventory/EmptyNamespaces_$today.csv /home/username/ClusterInventories/AllClusters_$today
+scp user@hostname1.comcast.net:/root/ClusterInventory/PopulatedNamespaces_$today.csv /home/username/ClusterInventories/AllClusters_$today
+scp user@hostname2.comcast.net:/root/ClusterInventory/PopulatedNamespaces_$today.csv /home/username/ClusterInventories/AllClusters_$today
+scp user@hostname2:/root/ClusterInventory/EmptyNamespaces_$today.csv /home/username/ClusterInventories/AllClusters_$today
+scp user@hostname3.comcast.net:/root/ClusterInventory/PopulatedNamespaces_$today.csv /home/username/ClusterInventories/AllClusters_$today
+scp user@hostname3.comcast.net:/root/ClusterInventory/EmptyNamespaces_$today.csv /home/username/ClusterInventories/AllClusters_$today
+scp user@hostname4.comcast.net:/root/ClusterInventory/PopulatedNamespaces_$today.csv /home/username/ClusterInventories/AllClusters_$today
+scp user@hostname4.comcast.net:/root/ClusterInventory/EmptyNamespaces_$today.csv /home/username/ClusterInventories/AllClusters_$today
+
+# Zip up for easier download to local
+tar -czf AllClusterReports_$today.tgz /home/username/ClusterInventories/*_$today.csv
