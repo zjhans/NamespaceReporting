28f9a5486e01e0e9e047115517ac6e1018ac6cdc
Zach H <55612469+zjhans@users.noreply.github.com>
2020-07-09 07:36:25 -0400

    Update NamespaceReport_AllWorkloads.sh

--- NamespaceReport_AllWorkloads.sh
+++ NamespaceReport_AllWorkloads.sh
@@ -2,43 +2,44 @@
 today=$(date +'%Y%m%d')
 
 # Cleanup previously day's run, if file is present
-if [ -f /root/ClusterInventory/AllActiveNamespaces.txt ]; then
+if [ -f /root/ClusterInventory/AllActiveNamespaces.txt ] 
+then
      rm -f /root/ClusterInventory/AllActiveNamespaces.txt
 fi
 
-# Generate a list of all current, active namespaces on the cluster
-oc get namespaces | awk '{print $1}' | grep -v NAME > /root/ClusterInventory/AllActiveNamespaces.txt
+# Generate a list of all active namespaces on the cluster
+oc get namespaces | awk '{print $1}' | grep -v NAME > /root/ClusterInventory/AllNamespaces.txt
 
 # Loop through active namespaces and find any with cronjobs - these are assumed to be in use
-for i in `cat /root/ClusterInventory/AllActiveNamespaces.txt`
+for i in `cat /root/ClusterInventory/AllNamespaces.txt`
      do CRONCNT=`oc get cronjobs -n $i | grep -v NAME | wc -l`
      if [ "$CRONCNT" -ge "1" ]
      then
-          echo "$i" >> /root/ClusterInventory/CLUSTNAME-CronjobNamespaces_$today.csv
+          echo "$i" >> /root/ClusterInventory/CLUSTNAME-Cronjobs_$today.csv
     else
-          echo "$i" >> /root/ClusterInventory/CLUSTNAME-NoCronjobsNamespaces_$today.csv
+          echo "$i" >> /root/ClusterInventory/CLUSTNAME-NoCronjobs_$today.csv
      fi
 done
 
 # Loop through namespaces without cronjobs and find any with pods - these are also assumed to be in use
-for i in `cat /root/ClusterInventory/HOC1-NoCronjobNamespaces_$today.csv`
+for i in `cat /root/ClusterInventory/HOC1-NoCronjobs_$today.csv`
     do PODCNT=`oc get pods -n $i | grep -v READY | grep 'Running\|Completed' | wc -l`
     if [ "$PODCNT" -ge "1" ]
     then
-        echo "$i" >> /root/ClusterInventory/CLUSTNAME-PodsNamespaces_$today.csv
+        echo "$i" >> /root/ClusterInventory/CLUSTNAME-Pods_$today.csv
     else
-        echo "$i" >> /root/ClusterInventory/CLUSTNAME-NoPodNamespaces_$today.csv
+        echo "$i" >> /root/ClusterInventory/CLUSTNAME-NoPods_$today.csv
     fi
 done
 
-# Loop through namespaces without pods or cronjobs and find any workloads - these may or may not be in use
-# Anything that has no active workloads/resources is assumed to be dead and no migration is required
-for i in `cat /root/ClusterInventory/HOC1-NoPodNamespaces_$today.csv`
+# Loop through namespaces without pods OR cronjobs and find any other active resources/workloads - these may or may not be in use
+# Any namespace that has no active workloads/resources is assumed to be dead
+for i in `cat /root/ClusterInventory/CLUSTNAME-NoPods_$today.csv`
     do RESCNT=`oc get all -n $i | grep -v NAMESPACE | wc -l`
     if [ "$RESCNT" -ge "1" ]
     then
-        echo "$i" >> /root/ClusterInventory/CLUSTNAME-ActResNamespaces_$today.csv
+        echo "$i" >> /root/ClusterInventory/CLUSTNAME-ActResources_$today.csv
     else
-        echo "$i" >> /root/ClusterInventory/CLUSTNAME-NoActResNamespaces_$today.csv
+        echo "$i" >> /root/ClusterInventory/CLUSTNAME-NoActResources_$today.csv
     fi
 done
